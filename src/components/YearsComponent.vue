42c09382f2b82c7cb53bd70184ccaef2ab33e9ca
Allan941709 <allansolorzano700@gmail.com>
2025-03-05 09:50:48 +0100

    Año de Carrera

--- src/components/YearsComponent.vue
+++ src/components/YearsComponent.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="years">
-      <label for="year-select" class="years__label">Select a year:</label>
-      <select id="year-select" name="year-select" v-model="selectedYear" class="years__select" @change="handleYearChange">
+  
+    <div class="Componentyears">
+      <label for="year-select" class="Componentyears__label">Elige un Año:</label>
+      <select id="year-select" name="year-select" v-model="selectedYear" class="Componentyears__select" @change="handleYearChange">
         <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
       </select>
     </div>
@@ -31,24 +32,69 @@
   </script>
   
   <style scoped>
-  .years {
+  .Componentyears {
     display: flex;
     flex-direction: column;
-    gap: 8px;
-    max-width: 300px;
-    margin: auto;
+    gap: 12px;
+    padding: 20px;
+    border-radius: 12px;
+    border: 1px solid rgba(17, 11, 11, 0.3);
+    transition: all 0.3s ease-in-out;
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 70%;
+    background-color: #fff;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   }
-  
-  .years__label {
-    font-size: 16px;
-    font-weight: bold;
+
+  .Componentyears:hover {
+    transform: translate(-50%, -50%) scale(1.05);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
   }
-  
-  .years__select {
-    padding: 8px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+
+  .Componentyears__label {
+    font-size: 4.5rem;
+    margin-bottom: 1rem;
+    text-shadow: 10px 10px 10px rgba(65, 64, 64, 0.846);  
+    color: #141414;
+    text-align: center;
+  }
+
+  .Componentyears__select {
+  padding: 0.5rem;
+  font-size: 1.2rem;
+  border-radius: 0.5rem; 
+  background-color: #f4f6f7; 
+  color: #333;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  appearance: none;
+}
+
+/* Cambiar color de fondo y texto cuando el select está en foco */
+.Componentyears__select:focus {
+  background-color: #ffe6e6; 
+  color: #d32f2f; 
+  border-color: #b71c1c; 
+  outline: none;
+}
+/*Opciones dentro del select */
+.Componentyears__select option {
+  max-height: 120px; 
+  overflow-y: auto;
+    background-color: white; 
+  color: black; 
+}
+/* Quitar el color azul de selección en algunos navegadores */
+.Componentyears__select option:hover,
+.Componentyears__select option:checked {
+  background-color: #d32f2f; 
+  color: white; 
+}
+
+  .Componentyears__select:hover {
+    border-color: #888;
   }
   </style>
-  
